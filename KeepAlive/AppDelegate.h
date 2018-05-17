1c7f13ca2ea38ed72b32b6f8631edd5302b00e7f
Zhu Dongfang <zhudongfang@cmcm.com>
2018-05-17 14:26:59 +0800

    Initial Commit

---
+++ KeepAlive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeepAlive
+//
+//  Created by 朱东方 on 2018/5/17.
+//  Copyright © 2018年 cmcm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KeepAlive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KeepAlive
+//
+//  Created by 朱东方 on 2018/5/17.
+//  Copyright © 2018年 cmcm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
